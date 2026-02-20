6e03bea40cf7e6a7525510cb3281930a1da30686
trombonelover67-oss <trombonelover67@gmail.com>
2026-02-19 21:25:26 -0600

    Create app.js for OvO, Drive Mad, and Tap Tap Shots games

---
+++ app.js
@@ -0,0 +1,26 @@
+// OvO Game
+function OvoGame() {
+    console.log('OvO game started!');
+    // Additional game code here...
+}
+
+// Drive Mad Game
+function DriveMadGame() {
+    console.log('Drive Mad game started!');
+    // Additional game code here...
+}
+
+// Tap Tap Shots Game
+function TapTapShotsGame() {
+    console.log('Tap Tap Shots game started!');
+    // Additional game code here...
+}
+
+// Initialize all games
+function startGames() {
+    OvoGame();
+    DriveMadGame();
+    TapTapShotsGame();
+}
+
+startGames();
